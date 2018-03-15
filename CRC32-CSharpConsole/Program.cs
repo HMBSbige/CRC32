75750e5469d62789f77e4783760e8a3986e1446d
HMBSbige <admin@bige0.com>
2018-03-15 14:52:26 +0800

    C# & C

---
+++ CRC32-CSharpConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace CRC32_CSharpConsole
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                var str = Console.ReadLine();
+                if (str == null)
+                {
+                    break;
+                }
+                var arrayOfBytes = Encoding.UTF8.GetBytes(str);
+                Console.WriteLine(Crc32.Crc32.Get(arrayOfBytes).ToString(@"X"));
+            }
+        }
+    }
+}
